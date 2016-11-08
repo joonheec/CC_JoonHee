ce11057261328ae505dac488c33cebd5cc9f7294
joonheec <joonheechoi@Joons-MacBook-Pro.local>
2016-11-08 11:07:30 -0500

    ss7

--- SS7_JoonHee/js/sketch.js
+++ SS7_JoonHee/js/sketch.js
@@ -22,10 +22,15 @@ function setup() {
   canvas = createCanvas(500,600);
   imageMode(CENTER);
   song1_image.loadPixels();
+  song2_image.loadPixels();
+  song3_image.loadPixels();
+  song1.play();
+  song2.pause();
+  song3.pause();
   volumeSlider = createSlider(0,2000,1000);
   volumeSlider.position(460, 560);
   button = createButton('>>');
-  button.position(600, 560);
+  button.position(700, 560);
   button.mousePressed(nextSong);
   
 }
@@ -34,29 +39,43 @@ function draw() {
   speed = volumeSlider.value()/1000;
   speed = constrain(speed,0,2);
   song1.rate(speed);
+  song2.rate(speed);
+  song3.rate(speed);
   fill(0,0,0);
-  text("Tempo", 45,520);
+  textAlign(CENTER);
+  text("Tempo", 50,520);
   text("-",0, 520);
   text("+",110, 520);
-  
+  console.log(state);
+  if (state == 'aurora'){
+
+    image(song1_image,250,250);
+    
+  }
+  if(state == 'second'){
+
+    image(song2_image,250,250);
+    
+  }
+  if(state =='third'){
+   
+    image(song3_image,250,250);
+    
+  }
+
   
 }
 function nextSong() {
   if (state == 'aurora'){
-    image(song1_image,250,250);
-    fill(255,255,255);
-    text("Press to play", width/2,height/2-100);
-    song1.pause();
+  
+    song1.stop();
     song3.pause();
     state = 'second';
     song2.play();
 
   }
   else if(state == 'second'){
-    image(song2_image,250,250);
-    fill(255,255,255);
-    text("Press to play", width/2,height/2-100);
-    song2.pause();
+    song2.stop();
     song1.pause();
     state = 'third';
 
@@ -65,10 +84,8 @@ function nextSong() {
     
   }
   else if(state == 'third'){
-    image(song3_image,250,250);
-    fill(255,255,255);
-    text("Press to play", width/2,height/2-100);
-    song3.pause();
+    
+    song3.stop();
     song2.pause();
     state = 'first';
     song1.play();
