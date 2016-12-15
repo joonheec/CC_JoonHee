f2a28d134f4c3d3ab15150c3a37e67922d5c2fc2
joonheec <joonheechoi@Joons-MacBook-Pro.local>
2016-12-15 02:20:17 -0500

    added sliders and fixed spectrum

--- Final Project/js/sketch.js
+++ Final Project/js/sketch.js
@@ -4,8 +4,15 @@ var lowerPeakDetect;
 var midPeakDetect; // mid range peak
 var higherPeakDetect; // higher range peak
 var soundFile;
-var binCount = 256;
+var binCount = 512;
 var particles =  new Array(binCount);
+var tempoSlider;
+var tempoValue;
+var ampSlider;
+var ampValue;
+var amp;
+var speed;
+var randomInt;
 
 //
 // variables to manage the three character images
@@ -15,24 +22,32 @@ var w1 = null;
 var w2 = null;
 var w3 = null;
 
-var minimumWidth1 = 300;
-var minimumWidth2 = 250;
-var minimumWidth3 = 180;
+var minimumWidth1 = 400;
+var minimumWidth2 = 300;
+var minimumWidth3 = 250;
 var maximumWidth1 = minimumWidth1 + (minimumWidth1 * .25);
 var maximumWidth2 = minimumWidth2 + (minimumWidth1 * .25);
-var maximumWidth3 = minimumWidth3 + (minimumWidth1 * .25);
+var maximumWidth3 = minimumWidth3 + (minimumWidth1 * .55);
 
 var imagePath1 = "../img/circle.png";
 var imagePath2 = "../img/circle.png";
 var imagePath3 = "../img/circle.png";
 
 var imageCache = {};
-//Particle Class
+//Particle Class and spectrum by Jason Sigal editted by me 
+//https://github.com/therewasaguy/p5-music-viz/tree/master/demos/04_fft_freq_spectrum
+//
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 var Particle = function(position) {
   this.position = position;
   this.scale = random(0, 1);
-  this.speed = createVector(0, random(0, 10) );
-  this.color = [random(50, 255), 0, random(30,255),random(40,99)];
+  this.speed = createVector(0, random(0, 7) );
+  this.color = [255, random(30,180), random(140,255),random(70,90)];
+
 }
 
 var theyExpand = 1;
@@ -43,12 +58,33 @@ Particle.prototype.update = function(someLevel) {
   if (this.position.y > height) {
     this.position.y = 0;
   }
-  this.diameter = map(someLevel, 0, 1, 0, 100) * this.scale * theyExpand;
+  this.diameter = map(someLevel, 0, 1, 0, 75) * this.scale * theyExpand;
+  if (lowerPeakDetect.isDetected){
+    this.diameter *= 1.03;
+  }
+  if (midPeakDetect.isDetected){
+    this.diameter *= 1.05;
+  }
+  if (higherPeakDetect.isDetected){
+    this.diameter *= 1.1;
+  }
 
 }
 
 Particle.prototype.draw = function() {
     noStroke();
+    // if(randomInt == 0){
+    //   this.color = [random(30,40), random(30,180), 255,random(20,90)];
+    // }
+    // else if(randomInt == 1){
+    //   this.color = [random(40,120), 255, random(180,255),random(70,90)];
+    // }
+    // else if(randomInt == 2){
+    //   this.color = [255, random(150,180), random(40,170),random(50,90)];
+    // }
+    // else{
+    //   this.color = [255, random(30,180), random(140,255),random(70,90)];
+    // }
     fill(this.color);
     ellipse(
       this.position.x, this.position.y,
@@ -71,17 +107,26 @@ function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   background(0);
   noStroke();
+  //make canvas drag and droppable
+  canvasDragDrop(cnv, newFile);
   fill(255);
   textAlign(CENTER);
   imageMode(CENTER);
   stroke(0);
+
+  //instantiate a slider
+  tempoSlider = createSlider(0,2000,1000);
+  tempoSlider.position(windowWidth/2, 50);
+  ampSlider = createSlider(0,1000,500);
+  ampSlider.position(windowWidth/2, 200);
+
   fft = new p5.FFT();
 
   lowerPeakDetect = new p5.PeakDetect(40,2000);
   midPeakDetect = new p5.PeakDetect(2000,4000);
   higherPeakDetect = new p5.PeakDetect(4000,10000);
 
-  fft = new p5.FFT(0.8, 1024);
+  fft = new p5.FFT(0.99, 1024);
   fft.setInput(soundFile);
 
   for (var i = 0; i < particles.length; i++) {
@@ -89,6 +134,18 @@ function setup() {
     var y = random(0, height);
     var position = createVector(x, y);
     particles[i] = new Particle(position);
+    if(randomInt == 0){
+      particles[i].color = [random(30,40), random(30,180), 255,random(20,90)];
+    }
+    else if(randomInt == 1){
+      particles[i].color = [random(40,120), 255, random(180,255),random(70,90)];
+    }
+    else if(randomInt == 2){
+      particles[i].color = [255, random(150,180), random(40,170),random(50,90)];
+    }
+    else{
+      particles[i].color = [255, random(30,180), random(140,255),random(70,90)];
+    }
   }
 }
 
@@ -98,8 +155,9 @@ function draw() {
   // update the background and UI elements
   //
   background(0);
-  
+  fill(255);
   text('click to play/pause', width/10, height/10);
+  text('drag and drop songs to load into visualizer',width/10,height/10+20);  
   var spectrum = fft.analyze(binCount);
 
   //
@@ -149,30 +207,40 @@ function draw() {
   //
   // update images width, height, x, y based on peak detection
   //
-  image(imageCache[imagePath1],windowWidth/2, windowHeight/2+lowerPeakDetect.energy *100,w1,w1);
+  image(imageCache[imagePath1],windowWidth/2, (height/2)+lowerPeakDetect.energy *100,w1,w1);
   image(imageCache[imagePath2], width/2 - (imageCache[imagePath2].width/2)-200, (height/2)  + midPeakDetect.energy *100, w2, w2);
   image(imageCache[imagePath3], width/2 - (imageCache[imagePath3].width/2) + maximumWidth3+200, (height/2 )  + higherPeakDetect.energy *100, w3, w3);
 
-  // image(imageCache[imagePath1], width/2 - (imageCache[imagePath1].width/2) - maximumWidth1, (height/2 - (imageCache[imagePath1].height/2)) + lowerPeakDetect.energy *100, w1, w1);
-
-  // image(imageCache[imagePath2], width/2 - (imageCache[imagePath2].width/2), (height/2 - (imageCache[imagePath2].height/2))  + midPeakDetect.energy *100, w2, w2);
-
-  // image(imageCache[imagePath3], width/2 - (imageCache[imagePath3].width/2) + maximumWidth3, (height/2 - (imageCache[imagePath3].height/2))  + higherPeakDetect.energy *100, w3, w3);
   for (var i = 0; i < binCount; i++) {
     var thisLevel = map(spectrum[i], 0, 255, 0, 1);
-
     // update values based on amplitude
-    particles[i].update( thisLevel );
+    particles[i].update(thisLevel);
 
-    // draw the particle in index = i
+    // draw the particle in index 
     particles[i].draw();
 
-    //update x coordinate of particle based on binCount
     particles[i].position.x = map(i, 0, binCount, 0, width * 2);
 
   }
+  //get value from slider 
+  speed = tempoSlider.value()/1000;
+  speed = constrain(speed,0,2);
+  amp = ampSlider.value()/1000;
+  amp = constrain(amp,0,1);
+  //set rate of song
+  soundFile.rate(speed);
+  soundFile.setVolume(amp);
 
+}
+function canvasDragDrop(cnv, newFile) {
+  cnv.drop(newFile);
+}
 
+function newFile(file) {
+  soundFile.dispose();
+  soundFile = loadSound(file);
+  fft.setInput(soundFile);
+  fft.analyze(soundFile);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -180,6 +248,7 @@ function windowResized() {
 // toggle play/stop when canvas is clicked
 function mouseClicked() {
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    randomInt = getRandomInt(0,3);
     if (soundFile.isPlaying() ) {
       soundFile.stop();
       binCount = 32;
